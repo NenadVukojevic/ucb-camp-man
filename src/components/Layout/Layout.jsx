75bc3659a20a8e25e1cabc9f58426ebe5cd12246
NenadVUkojevic <nenad.vukojevic@gmail.com>
2021-09-17 11:31:36 +0200

    User management

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,7 +3,6 @@ import { Route, Switch } from 'react-router-dom';
 import CRMCampaigns from '../Campaign/CRMCampaigns';
 import EditCampaign from '../Campaign/EditCampaign';
 import FooterComponent from '../Header/FooterComponent';
-import HeaderComponent from '../Header/HeaderComponent';
 import TableContent from '../TableContent/TableContent';
 import "bootstrap/dist/css/bootstrap.min.css"
 import './Layout.css';
@@ -18,6 +17,14 @@ import GroupOfTerminals from '../Terminals/GroupOfTerminals';
 import EditGroupOfTerminal from '../Terminals/EditGroupOfTerminal';
 import GroupOfBinRanges from '../BinRanges/GroupOfBinRanges';
 import EditBinRangeGroup from '../BinRanges/EditBinRangeGroup';
+import ListOfTerminals from '../Terminals/ListOfTerminals';
+import EditTerminal from '../Terminals/EditTerminal';
+import Groups from '../UserManagement/Groups';
+import EditGroup from '../UserManagement/EditGroup';
+import Roles from '../UserManagement/Roles';
+import EditRole from '../UserManagement/EditRole';
+import Users from '../UserManagement/Users';
+import EditUser from '../UserManagement/EditUser';
 
 
 
@@ -26,6 +33,7 @@ import EditBinRangeGroup from '../BinRanges/EditBinRangeGroup';
 
 class Layout extends Component {
     render() {
+        
         const isLoggedIn = Boolean(localStorage.getItem("loggedIn") === 'true');
         console.log("layout", isLoggedIn);
 
@@ -62,7 +70,21 @@ class Layout extends Component {
                         <Route path="/edit-binRangeGroup/" exact component={EditBinRangeGroup} />
                         <Route path="/edit-binRangeGroup/:id" component={EditBinRangeGroup} />
 
+                        <Route path="/terminals" component={ListOfTerminals} />
+                        <Route path="/edit-terminal" exact component={EditTerminal} />
+                        <Route path="/edit-terminal/:id" component={EditTerminal} />
 
+                        <Route path="/groups" component={Groups} />
+                        <Route path="/edit-group" exact component={EditGroup} />
+                        <Route path="/edit-group/:id" component={EditGroup} />
+
+                        <Route path="/roles" component={Roles} />
+                        <Route path="/edit-role" exact component={EditRole} />
+                        <Route path="/edit-role/:id" component={EditRole} />
+
+                        <Route path="/users" component={Users} />
+                        <Route path="/edit-user" exact component={EditUser} />
+                        <Route path="/edit-user/:id" component={EditUser} />
 
                         <Route path="/head" component={TableContent}></Route>
 
